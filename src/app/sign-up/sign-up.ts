1a928ec36a780673a461b3b54de7301048903f3c
Niaina <aina.etech@gmail.com>
2025-12-03 16:35:14 +0300

    feat: création et intégration composant Input-field

--- src/app/sign-up/sign-up.ts
+++ src/app/sign-up/sign-up.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SocialButton } from '../shared/components/social-button/social-button';
+import { InputField } from '../shared/components/input-field/input-field';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [SocialButton],
+  imports: [SocialButton,InputField],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.scss',
 })
